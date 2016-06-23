24139339c4bd976bddcb7176bce28ef0f95bca55
Shawn L <shawnl@istuary.com>
2016-06-23 11:54:12 -0700

    Deployment Build - Thu Jun 23 11:54:12 PDT 2016

---
+++ dist/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var pjson = require('./package.json');
+
+var app = express();
+
+app.use('/client_version', function(req, res){
+  res.end(pjson.version);
+});
+app.use('/fonts', express.static(path.join(__dirname, 'fonts')));
+app.use('/favicon.ico', express.static(path.join(__dirname, 'favicon.ico')));
+app.use('/app.full.min.css', express.static(path.join(__dirname, 'app.full.min.css')));
+app.use('/app.full.min.js', express.static(path.join(__dirname, 'app.full.min.js')));
+
+app.use('*', function(req,res){
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+var server = http.createServer(app);
+server.listen(3001, '0.0.0.0');
